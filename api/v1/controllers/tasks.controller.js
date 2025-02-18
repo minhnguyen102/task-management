7a7300f3e5131c2f5d36d508bd0710b415c3eda6
CodeMnh <minhkhac1002@gmail.com>
2025-02-18 15:38:35 +0700

    api sap xep theo tieu chi

--- api/v1/controllers/tasks.controller.js
+++ api/v1/controllers/tasks.controller.js
@@ -1,12 +1,30 @@
 const Task = require("../models/taks.model")
+// [GET] /api/v1/tasks
 module.exports.index = async (req, res) =>{
-    const task = await Task.find({
+    let find = {
         deleted : false
-    });
+    }
 
+    // filterStatus
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    // Sort
+    console.log(req.query);
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey] = req.query.sortValue
+    }
+    // End Sort
+    
+
+    const task = await Task.find(find)
+                        .sort(sort)
     res.json(task)
 }
 
+// [GET] /api/v1/tasks/:id
 module.exports.detail = async (req, res) => {
     const id = req.params.id;
     const task = await Task.find({
