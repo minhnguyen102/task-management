c6b3b1621fa1769fed42570132bb7698badd36bf
CodeMnh <minhkhac1002@gmail.com>
2025-02-18 21:14:53 +0700

    api change-status

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/tasks.controller")
 
 router.get('/', controller.index)
 router.get('/detail/:id', controller.detail)
+router.patch('/change-status/:id', controller.changeStatus)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,25 +2,18 @@ const express = require('express')
 const database = require("./config/database")
 require("dotenv").config()
 const routesApiV1 = require("./api/v1/routes/index.route")
+const bodyParser = require("body-parser")
 
 const app = express()
 const port = 3000
 
+// connect database
 database.connect();
 
+// parse application/json
+app.use(bodyParser.json())
+
 routesApiV1(app);
-
-
-// app.get('/task', async (req, res) => {
-//     const taks = await Taks.find({
-//         deleted : false
-//     });
-//     console.log(taks);
-
-//     res.json(taks)
-// })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
